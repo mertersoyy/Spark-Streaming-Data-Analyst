0158de4fcc12fe86f46f893d28ae2c415804ab68
Mert Ersoy <96493388+mertersoyy@users.noreply.github.com>
2024-01-03 14:09:00 +0300

    q2 solution.sql

--- Mid_Proje/q2 solution.sql
+++ Mid_Proje/q2 solution.sql
@@ -1,11 +1,7 @@
-/*
-
-select * from (
-select *, row_number() over (partition by subscription_status order by total_sales desc) as sales_rank from (
-SELECT avg(Review_Rating) as Avg_Review,Category, count(customer_id) as Total_Sales,Subscription_Status 
+SELECT * FROM (
+SELECT *, row_number() over (partition BY subscription_status ORDER BY total_sales DESC) AS sales_rank FROM (
+SELECT avg(Review_Rating) AS Avg_Review,Category, COUNT(customer_id) AS Total_Sales,Subscription_Status 
 FROM skilled-module-406113.mid_proje.shopping_behavior
 GROUP BY Category,Subscription_Status
 ORDER BY Total_Sales DESC ))
 WHERE sales_rank <3
-
-*/
