bece85ae296e087c1c5a9eba6be00146677d6068
Mert Ersoy <96493388+mertersoyy@users.noreply.github.com>
2024-01-03 14:09:34 +0300

    q3 solution.sql

--- Mid_Proje/q3 solution.sql
+++ Mid_Proje/q3 solution.sql
@@ -1,13 +1,11 @@
-/*
-SELECT Shipping_Type,count(*) AS TOTAL_ORDER,sum(Purchase_Amount) AS PURCHASE_AMOUNT,avg(Review_Rating) as REVIEW 
+
+SELECT Shipping_Type,COUNT(*) AS TOTAL_ORDER,sum(Purchase_Amount) AS PURCHASE_AMOUNT,avg(Review_Rating) AS REVIEW 
 FROM `skilled-module-406113.mid_proje.shopping_behavior` 
 GROUP BY Shipping_Type
 ORDER BY PURCHASE_AMOUNT DESC
-*/
 
-/*
-SELECT avg(Review_Rating) as AVG_Review,Shipping_Type,Frequency_Purchases,count(*) as Total_Sales FROM `skilled-module-406113.mid_proje.shopping_behavior` 
+
+
+SELECT avg(Review_Rating) AS AVG_Review,Shipping_Type,Frequency_Purchases,COUNT(*) AS Total_Sales FROM `skilled-module-406113.mid_proje.shopping_behavior` 
 GROUP BY Shipping_Type, Frequency_Purchases
-Order By avg(Review_Rating) asc
-
-*/
+ORDER BY avg(Review_Rating) ASC
